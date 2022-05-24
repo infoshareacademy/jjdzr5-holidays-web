c23887f1bfdcf76e322c0065e02168096b337a87
Zbigniew Kupiec <zk-design@outlook.com>
2022-05-24 19:46:58 +0200

    update file pom.xml and fix name folder

--- holidays-web/src/main/java/org/isa/holidaysweb/controller/HolidaysController.java
+++ holidays-web/src/main/java/org/isa/holidaysweb/controller/HolidaysController.java
@@ -1,4 +1,4 @@
-package org.isa.holidaysweb.conrtoller;
+package org.isa.holidaysweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
